0076c51a98627d7a43cb6583470ee9f0acf1f58c
Razer <therazerhub@gmail.com>
2024-11-12 19:51:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,7 +100,7 @@ PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 # Streaming
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002476392915")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1001949340728")
 if len(BIN_CHANNEL) == 0:
     logging.error('BIN_CHANNEL is missing, exiting now')
     exit()
